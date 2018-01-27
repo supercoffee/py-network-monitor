8458317e74772286c2963b4e256987b10403de8a
Benjamin Daschel <coffeemaxed@gmail.com>
2018-01-27 10:59:52 -0800

    launch Agent as user instead of root

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ install() {
     sed -e "s|{{ install_dir }}|$PWD|g" -e "s|{{ python_path }}|$python_path|g" -e "s|{{ env_path }}|$PATH|g" |\
     tee ~/Library/LaunchAgents/com.bendaschel.py-network-monitor.plist
 
-    sudo launchctl unload ~/Library/LaunchAgents/com.bendaschel.py-network-monitor.plist
-    sudo launchctl load ~/Library/LaunchAgents/com.bendaschel.py-network-monitor.plist
+    launchctl unload ~/Library/LaunchAgents/com.bendaschel.py-network-monitor.plist
+    launchctl load ~/Library/LaunchAgents/com.bendaschel.py-network-monitor.plist
 }
 
 install
