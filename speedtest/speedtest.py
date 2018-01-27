ea09275aa7bf1f628441c06ded3486c182deeed0
Benjamin Daschel <coffeemaxed@gmail.com>
2018-01-26 23:34:58 -0800

    basic web dashboard with download speed graph

--- speedtest/speedtest.py
+++ speedtest/speedtest.py
@@ -25,7 +25,8 @@ def import_results(results_dict):
 
     conn = sqlite3.connect('speed-test-results.db')
     conn.execute("""INSERT INTO servers VALUES (:id, :latency, :name, :url, :country, :lon, :lat, :cc, :host, :sponsor, :url2, :d)""", server)
-    conn.execute("""INSERT INTO test_results VALUES (:bytes_sent, :download, :timestamp, :bytes_received, :ping, :upload, :server)""", results_dict)
+    conn.execute("""INSERT INTO test_results  (bytes_sent, download, timestamp, bytes_received, ping, upload, server)
+                    VALUES (:bytes_sent, :download, :timestamp, :bytes_received, :ping, :upload, :server)""", results_dict)
     conn.commit()
     conn.close()
 
@@ -35,14 +36,32 @@ def prepare_db():
     conn = sqlite3.connect('speed-test-results.db')
 
     conn.execute("""
-        CREATE TABLE test_results (bytes_sent REAL, download REAL, timestamp DATETIME, bytes_received REAL, ping REAL, upload REAL, server INTEGER)
+        CREATE TABLE test_results (id INTEGER PRIMARY KEY , bytes_sent REAL, download REAL, timestamp DATETIME,
+        bytes_received REAL, ping REAL, upload REAL, server INTEGER)
         """)
     conn.execute("""
         CREATE TABLE servers (id INTEGER, latency REAL, name TEXT, url TEXT, country TEXT, longitude REAL, latitude REAL, country_code TEXT,
                                 host TEXT, sponsor TEXT, url2 TEXT, d_what_is_this REAL)
         """)
+
     conn.commit()
     conn.close()
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+def get_all_speedtest_results():
+    conn = sqlite3.connect('speed-test-results.db')
+    conn.row_factory = dict_factory
+    cursor = conn.cursor()
+    cursor.execute("""SELECT * from test_results""")
+    results = [dict(r) for r in cursor]
+    cursor.close()
+    conn.close()
+    return results
 
 
 
--- web/index.py
+++ web/index.py
@@ -1,15 +1,21 @@
 from flask import Flask
+from flask import jsonify
+from speedtest import speedtest
+from flask import render_template
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def hello_world():
-    return 'hello world!'
+    return render_template('dashboard.html')
 # TODO: return dashboard page
 
 @app.route('/speedtests/')
 def speedtests():
-    pass
+    results = speedtest.get_all_speedtest_results()
+    return jsonify(results)
+
 
 @app.route('/servers/')
 def servers():
